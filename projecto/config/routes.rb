9d1a8ed57150d6ef5b0222e6f23a0d5f8363b936
Eric Webster <quanta@Eric-Websters-MacBook-Pro.local>
2013-09-01 01:00:47 -0400

    created user scaffold/main controller

--- projecto/config/routes.rb
+++ projecto/config/routes.rb
@@ -1,4 +1,7 @@
 Projecto::Application.routes.draw do
+  resources :users
+
+  get "main/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
