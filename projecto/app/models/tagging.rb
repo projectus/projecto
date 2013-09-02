87b6cf17876d9fdc847b83fc67d16d6223857f7b
Eric Webster <quanta@Eric-Websters-MacBook-Pro.local>
2013-09-01 21:39:37 -0400

    easier to navigate

--- projecto/app/models/tagging.rb
+++ projecto/app/models/tagging.rb
@@ -1,4 +1,8 @@
 class Tagging < ActiveRecord::Base
   belongs_to :tag
   belongs_to :project
+
+  # Make sure a tag is only used once on a project
+  validates_uniqueness_of :tag_id, :scope => :project_id
+
 end
