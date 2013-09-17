84f07f9dcd838744291ae5a409c3553cb4770659
Eric Webster <quanta@Eric-Websters-MacBook-Pro.local>
2013-09-17 19:03:40 -0300

    unsubscribe

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,16 +1,21 @@
 class SubscriptionsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :index
 
   def index
 	  @user = User.find(params[:user_id])
 	  @subscriptions = @user.subscriptions
-	  
   end
 
   def destroy
 	  @subscription = Subscription.find(params[:id])
-	  feed = @subscription.activity_feed
 	  user = @subscription.user
+	
+	  unless current_user == user
+      redirect_to :back, alert: 'This is not your subscription!'
+      return
+    end
+
+	  feed = @subscription.activity_feed
 	  @subscription.destroy
 		respond_to do |format|
 	    format.html { redirect_to user_subscriptions_path(user), notice: "Unsubscribed from #{feed.name}!" }
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -5,6 +5,7 @@ class Subscription < ActiveRecord::Base
   # Make sure a user only subscribes once to a feed
 	validates :user_id, :uniqueness => { :scope => :activity_feed_id }
 
+  # Return the activities listed in the feed this subscriptions refers to
   def activities
 	  activity_feed.activities
 	end
