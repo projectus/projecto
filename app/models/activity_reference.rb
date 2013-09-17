6c5101a2271d955bad73b73cc6b22b21651028ba
Eric Webster <quanta@Eric-Websters-MacBook-Pro.local>
2013-09-17 16:07:36 -0300

    activity

--- app/models/activity_reference.rb
+++ app/models/activity_reference.rb
@@ -1,6 +1,9 @@
 class ActivityReference < ActiveRecord::Base
+	
   belongs_to :referenceable, polymorphic: true
   belongs_to :activity
+
+  # VALIDATION #####################################################
 
   # Make sure a reference is only used once in an activity
   validates_uniqueness_of :activity_id, :scope => [:referenceable_id,:referenceable_type]
@@ -10,4 +13,15 @@ class ActivityReference < ActiveRecord::Base
 
   validates_presence_of :activity
   validates_presence_of :referenceable
+
+  # AFTER DESTROY ###################################################
+
+  # Upon destroying the references in the activity, 
+  # remove the activity if there are no references left
+  after_destroy :remove_empty_activity
+
+  private
+    def remove_empty_activity
+	    activity.destroy if activity.activity_references.empty?
+	  end
 end
