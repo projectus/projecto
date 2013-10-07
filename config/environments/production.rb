629c70b53e51f546701fd7dbf6e56fcf43efacf5
Eric Webster <quanta@Eric-Websters-MacBook-Pro.local>
2013-10-07 16:10:41 -0400

    bucket

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,8 +80,8 @@ Projecto::Application.configure do
 
 	config.paperclip_defaults = {
 	  :storage => :s3,
+	  :bucket => ENV['AWS_BUCKET'],
 	  :s3_credentials => {
-	    :bucket => ENV['AWS_BUCKET'],
 	    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
 	    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
 	  }
