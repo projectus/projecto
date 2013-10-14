bdaa42f6bb482a8af6ca6789c1e98e9a78b4740c
Eric Webster <quanta@Eric-Websters-MacBook-Pro.local>
2013-10-13 22:16:53 -0400

    m

--- app/views/collaboration_invitations/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @invitation, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Projecto::Application.routes.draw do
+	
 	get "gallery/:id", to: 'gallery#show', as: :gallery
   post "gallery/:id/upload", to: 'gallery#upload', as: :gallery_upload
+
   get "project/:id/collaborations", to: 'project_collaboration_info#collaborations', as: :project_collaborations
   get "project/:id/applications", to: 'project_collaboration_info#applications', as: :project_applications
   get "project/:id/invitations", to: 'project_collaboration_info#invitations', as: :project_invitations
@@ -44,8 +46,8 @@ Projecto::Application.routes.draw do
 
   # APPLICATIONS AND INVITATIONS ###########################
 
-  resources :collaboration_invitations, except: [:index, :new, :destroy, :edit]
-  resources :collaboration_applications, except: [:index, :new, :destroy, :edit]
+  resources :collaboration_invitations, only: [:show, :create, :update]
+  resources :collaboration_applications, only: [:show, :create, :update]
   resources :comments, only: [:create]
   resources :news_posts, only: [:create]
 
