e76bf68e7e44aea3ca34431fa8f9eb81d8f4054e
Eric Webster <quanta@Eric-Websters-MacBook-Pro.local>
2013-10-15 00:04:26 -0400

    m

--- app/controllers/beta_users_controller.rb
+++ app/controllers/beta_users_controller.rb
@@ -1,12 +1,13 @@
 class BetaUsersController < ApplicationController
   def create
 	  @beta_user = BetaUser.new(params.require(:beta_user).permit(:email))
-    @beta_user.save
 
     respond_to do |format|
 	    if @beta_user.save
         format.js
-      end
+      else
+	      format.js
+	    end
     end
   end
 end
